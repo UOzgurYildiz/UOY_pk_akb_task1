7e6d35d16da337c254d3eb35c92ec665c91e11f7
UOzgurYildiz <uozguryildiz@hotmail.com>
2023-12-26 14:45:43 +0300

    delete added

--- Task1_api/Controllers/BookController.cs
+++ Task1_api/Controllers/BookController.cs
@@ -62,8 +62,16 @@ public class BookController : ControllerBase
         return Ok();
     }
 
-    //[HttpDelete]
-    //public IActionResult DeleteBook()
+    [HttpDelete("{id}")]
+    public IActionResult DeleteBook(int id)
+    {
+        var book = BookList.SingleOrDefault(x => x.Id == id);
+        if(book is null)
+            return BadRequest();
+
+        BookList.Remove(book);
+        return Ok();
+    }
     
 
     //[HttpPatch]
